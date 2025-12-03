29bb8c40cb968d7baad8bfd535bde7585d640d3b
nolanearl <nolanearl@pm.me>
2025-12-02 23:39:59 -0500

    small cleanup

--- two/two.py
+++ two/two.py
@@ -6,24 +6,16 @@ with open('../one/input.txt') as file:
         dial_distance = int(line[1:])
         if dial_direction == 'R':
             overflow_location = (location + dial_distance)
-            # print('Overflow location ' + str(overflow_location))
             if overflow_location > 100:
-                # print('overflowed 1 ' + str(overflow_location))
                 finishing_zeroes += (1 * (overflow_location // 100))
-                # print('overflowed 1 start ' + str(location))
-                # print('overflowed 1 fz ' + str(finishing_zeroes))
             location = overflow_location % 100
         else:
             overflow_location = location - dial_distance
-            # print('Overflow location ' + str(overflow_location))
             if overflow_location < 0 and location != 0:
-                # print('overflowed 2 ' + str(overflow_location))
                 finishing_zeroes += abs(1 * overflow_location // 100)
-                # print('overflowed 2 fz ' + str(finishing_zeroes))
             location = overflow_location % 100
         if location == 0:
             finishing_zeroes += 1
-        # print('location ' + str(location) + ' finishing zeroes ' + str(finishing_zeroes))
         print(dial_direction + ' '+ str(dial_distance) + ' '+ str(location))
         print(finishing_zeroes)
     print(finishing_zeroes)
