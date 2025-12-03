cff5f44784f32f30833c0dfbd090a8ff648ff96a
nolanearl <nolanearl@pm.me>
2025-12-02 22:45:55 -0500

    Solution for day 1 part 1

--- one/one.py
+++ one/one.py
@@ -1,37 +1,37 @@
 
-def parse_input(dial_movement):
-    dial_direction = dial_movement[0]
-    if dial_direction not in ('L', 'R'):
-        print('--INVALID DIRECTION IN INPUT FILE--')
-        exit(1)
-    dial_distance = int(dial_movement[1:])
-    return dial_direction, dial_distance
+class DayOne:
+    @staticmethod
+    def parse_input(dial_movement):
+        dial_direction = dial_movement[0]
+        if dial_direction not in ('L', 'R'):
+            print('--INVALID DIRECTION IN INPUT FILE--')
+            exit(1)
+        dial_distance = int(dial_movement[1:])
+        return dial_direction, dial_distance
 
-def spin_dial(starting_location, spin_direction, change):
-    if spin_direction == 'R':
-        return (starting_location + change) % 100
-    elif spin_direction == 'L':
-        ending_location = starting_location - change
-        if ending_location == 0:
-            return 0
-        elif ending_location < 0:
-            return 100 - abs(ending_location) % 100
-        else:
-            return ending_location
-    return '--INVALID INPUT--'
+    @staticmethod
+    def spin_dial(location, spin_direction, change):
+        if spin_direction == 'R':
+            return (location + change) % 100
+        elif spin_direction == 'L':
+            return (location - change) % 100
+        return '--INVALID INPUT--'
 
-def print_movement(change, end_location):
-   print('The dial is rotated ' + change + ' to point at ' + str(end_location))
+    @staticmethod
+    def print_movement(change, end_location):
+       print('The dial is rotated ' + change + ' to point at ' + str(end_location))
 
-# with open('./one/input.txt', 'r') as file:
-#     location = 50
-#     finishing_zeroes = 0
-#     for line in file:
-#         direction, distance = parse_input(line)
-#         location = spin_dial(location, direction, distance)
-#         print_movement(line, location)
-#         if location == 0:
-#             finishing_zeroes += 1
-#     print('YOUR ANSWER IS - ' + str(finishing_zeroes))
-#
-#
+    def __init__(self):
+        self.current_location= 50
+        self.finishing_zeroes = 0
+        with open('./one/input.txt', 'r') as file:
+            for line in file:
+                direction, distance = self.parse_input(line)
+                self.current_location = self.spin_dial(self.current_location, direction, distance)
+                self.print_movement(line, self.current_location)
+                if self.current_location== 0:
+                    self.finishing_zeroes += 1
+            print('YOUR ANSWER IS - ' + str(self.finishing_zeroes))
+
+if __name__ == '__main__':
+    one = DayOne()
--- one/test_one.py
+++ one/test_one.py
@@ -1,48 +1,76 @@
 import unittest
-from one import one
 
+from one.one import DayOne
 
 class Test(unittest.TestCase):
-
     def test_parse_input_happy_path(self):
-        self.assertEqual(('L', 100), one.parse_input("L100"))
+        day_one= DayOne()
+        self.assertEqual(('L', 100), day_one.parse_input("L100"))
 
     def test_parse_input_invalid_direction(self):
+        day_one = DayOne()
         with self.assertRaises(SystemExit) as ec:
-            one.parse_input("S100")
+            day_one.parse_input("S100")
             self.assertEqual(1, ec.exception.code)
 
     def test_parse_input_no_direction(self):
+        day_one = DayOne()
         with self.assertRaises(SystemExit) as ec:
-            one.parse_input("100")
+            day_one.parse_input("100")
             self.assertEqual(1, ec.exception.code)
 
     def test_spin_dial_right_no_overflow(self):
-        result = one.spin_dial(20, 'R', 13)
+        day_one = DayOne()
+        result = day_one.spin_dial(20, 'R', 13)
         self.assertEqual(33, result)
 
     def test_spin_dial_right_overflow(self):
-        result = one.spin_dial(99, 'R', 13)
+        day_one = DayOne()
+        result = day_one.spin_dial(99, 'R', 13)
         self.assertEqual(12, result)
 
     def test_spin_dial_right_large_overflow(self):
-        result = one.spin_dial(99, 'R', 313)
+        day_one = DayOne()
+        result = day_one.spin_dial(99, 'R', 313)
         self.assertEqual(12, result)
 
     def test_spin_dial_left_no_overflow(self):
-        result = one.spin_dial(99, 'L', 13)
+        day_one = DayOne()
+        result = day_one.spin_dial(99, 'L', 13)
         self.assertEqual(86, result)
 
     def test_spin_dial_left_overflow(self):
-        result = one.spin_dial(0, 'L', 13)
+        day_one = DayOne()
+        result = day_one.spin_dial(0, 'L', 13)
         self.assertEqual(87, result)
 
     def test_spin_dial_left_large_overflow(self):
-        result = one.spin_dial(0, 'L', 213)
+        day_one = DayOne()
+        result = day_one.spin_dial(0, 'L', 213)
         self.assertEqual(87, result)
 
     def test_spin_dial_left_zero(self):
-        result = one.spin_dial(1, 'L', 1)
+        day_one = DayOne()
+        result = day_one.spin_dial(1, 'L', 1)
         self.assertEqual(0, result)
+
+    def test_example(self):
+        day_one = DayOne()
+        finishing_zeroes = 0
+        current_location = 50
+        with open('./one/example.txt', 'r') as file:
+            for line in file:
+                instruction, expected_result = line.split('-')
+                direction, distance = day_one.parse_input(instruction)
+                current_location = day_one.spin_dial(current_location, direction, distance)
+                if current_location == 0:
+                    finishing_zeroes += 1
+                self.assertEqual(int(expected_result), current_location)
+                print(instruction+'-'+str(current_location))
+        self.assertEqual(finishing_zeroes, 3)
+        print('YOUR ANSWER IS - ' + str(finishing_zeroes))
+
+
+
 
 
